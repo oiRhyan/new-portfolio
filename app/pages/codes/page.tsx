d7096a0962dd1754424ef865cdf5e75aa4f71020
Rhyan Araujo Chaves <123249383+oiRhyan@users.noreply.github.com>
2024-08-27 19:07:10 -0300

    Project Update
    New project add.

--- app/pages/codes/page.tsx
+++ app/pages/codes/page.tsx
@@ -6,7 +6,7 @@ import styles from "@/app/Glass.module.css"
 
 const StringoDesc = "Projeto desenvolvido apartir da API do Spotify, este projeto conta com um sistema de login integrado ao Spotify que possibilita a sincronização de contas e playlist do usúario em questão, além de poder trocar de faixas de maneira ilimitada." 
 const WhatIfDesc = "Game de plataforma mobile desenvolvido para Android com o entuito de ensinar e fortalecer os conceitos de meio ambiente para crianças e adolescentes, desenvolvido durante o TCC do curso técnico em informática. Disponivel agora na Gamejolt."
-const NetflixDesc = "Interface desenvolvida durante o curso de Desenvolvimento Android : Java & Kotlin, tem o objetivo de realizar uma clonagem da interface netflix de dispositivos móveis através do ConstraintLayout usando recursos como MaterialDesigner3 e Guidelines."
+const NetflixDesc = "Aplicativo desenvolvido durante um teste técnico ministrado pela empresa Bipa. Seu objetivo é recuperar uma lista de Nodes apartir de uma API via retrofit e converter os dados de Sat para Bitcoin, e Unix para data e horas das ultimas atualização de cada node."
 const CalculadoraIMC = "Calculadora de IMC desenvolvida durante o curso de Desenvolvimento Android : Java & Kotlin, o usúario pode inserir sua altura e peso atual para que o aplicativo possa fazer uma comparação de status e então apresentar um resultado em sua tela de resultados."
 const JokenPoText = "Jogo Jokenpo adaptado para mobile utilizando Java, o jogo consiste em que o jogador opite por uma das três opções pedra, papel ou tesoura, em seguida, o código da CPU irá randomizar uma opção de escolha e ao fim será comparada com o do jogador."
 const AlcoolText = "Este projeto é mais um dos vários desenvolvidos durante o curso de desenvolvimento para Android. Seu objetivo é permitir que o usuário insira informações sobre os preços da gasolina e do álcool, assim podendo encontrar o melhor custo beneficio."
@@ -28,7 +28,7 @@ export default function Code() {
             <div className="flex gap-5">
               <CardP title={"Stringo: Concept Music App"} techs={"https://skillicons.dev/icons?i=react,nextjs,tailwind"} url={"/images/banner-1-image.png"} desc={StringoDesc} link="https://github.com/oiRhyan/stringo-app-music" />
               <CardP title={"What If World Ended"} techs={"https://skillicons.dev/icons?i=cs,unity,motion"} url={"/images/what-if-banner.png"} desc={WhatIfDesc} link="https://gamejolt.com/games/whatifworld/667617"/>
-              <CardP title={"Netflix: Interface Conceitual"} techs={"https://skillicons.dev/icons?i=java,androidstudio,motion"} url={"/images/NetflixBanner.png"} desc={NetflixDesc} link="https://github.com/oiRhyan/app-netflix-concept" />
+              <CardP title={"Bipa: Conding Challenge"} techs={"https://skillicons.dev/icons?i=kotlin,androidstudio"} url={"/images/bipa.png"} desc={NetflixDesc} link="https://github.com/oiRhyan/bipa-coding-challenge-app" />
               <CardP title={"Calculadora de IMC para Android"} techs={"https://skillicons.dev/icons?i=kotlin,androidstudio,motion"} url={"/images/imc_banner.png"} desc={CalculadoraIMC} link="https://github.com/oiRhyan/app-calculadora-imc" />
               <CardP title={"Jokenpo para Android"} techs={"https://skillicons.dev/icons?i=java,androidstudio,motion"} url={"/images/jokenpo-mobile.png"} desc={JokenPoText} link={Jokenpo} />
               <CardP title={"Álcool ou Gasolina : Android App"} techs={"https://skillicons.dev/icons?i=kotlin,androidstudio,motion"} url={"/images/alcool.png"} desc={AlcoolText} link={Gasolina} />
